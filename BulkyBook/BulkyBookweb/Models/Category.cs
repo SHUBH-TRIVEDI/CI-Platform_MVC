435db959bd8fc6bb30d622b3b0e98ca905176fed
SHUBH-TRIVEDI <trivedishubh21@gmail.com>
2023-02-14 17:16:25 +0530

    ci plateform mvc uploaded

---
+++ BulkyBook/BulkyBookweb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookweb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String? Name { get; set; }
+
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
